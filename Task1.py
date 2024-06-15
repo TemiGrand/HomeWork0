5f839a04bb87fda39b36bec74ba1c5bdb4e3f64b
TemiGrand <125796794+TemiGrand@users.noreply.github.com>
2024-06-16 02:24:17 +0300

    Add files via upload

---
+++ Task1.py
@@ -0,0 +1,7 @@
+first = 73
+second = 52
+summa = first + second
+diff = first - second
+print('Даны числа:', first, 'и', second,
+      '\nСумма =', summa,
+      '\nРазность =', diff)
---
+++ Task2.py
@@ -0,0 +1,6 @@
+first = 73
+second = 52
+third = 3
+mean = (first + second + third)/3
+print('Даны числа:', str(first) + ',', second, 'и', third,
+      '\nСреднее арифметическое =', mean)
